1098b55439adecec372fbb9ac238d860b295f386
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-09 18:15:36 +0530

    Add C program to print 'Hello Sarah' multiple times to see how next line works

---
+++ C/day_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+  printf("Hello Sarah \n");
+  printf("Hello Sarah \n");
+  printf("Hello Sarah \n");
+  printf("Hello Sarah \n");
+  return 0;
+}
+//\n is used to place the next line of text on the line below the first line of text 
