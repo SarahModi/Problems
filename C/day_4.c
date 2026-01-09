a44bd4514880afff650fc1d5dd5332eb1a407c3b
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-09 18:25:19 +0530

    Add day_4.c with cases in C to print OUTPUT

---
+++ C/day_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+  int age = 27;
+  float total_price = 204.5;
+  char hashtag = '#';
+  printf("age is %d \n", age);
+  printf("loss is %f \n", total_price);
+  printf("symbol is %c \n", hashtag);
+  return 0;
+}
