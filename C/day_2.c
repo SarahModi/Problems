326e66ba8fef38853f86ec5c5c095684a5e028a6
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-09 18:03:54 +0530

    Add day_2.c with variable declarations

---
+++ C/day_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+ int number = 27; //int for integer
+ char star = '*'; //char is for character
+ float pi = 3.14; //float is for decimal values
+  return 0;
+}
+//Variables are case sensitive
+/*
+int a = 2
+int A = 4
+both a and A are different containers
+*/
