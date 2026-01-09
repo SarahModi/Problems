50e5fe549d9994bc253a054e77c80bda2c0d261f
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-09 18:45:48 +0530

    Add string manipulation examples in day_1.py
    Added multiple print statements demonstrating string formatting and variable usage.

---
+++ Python/day_1.py
@@ -0,0 +1,11 @@
+X = "Hello"
+Y = "Sarah"
+
+print("Hello Sarah")
+print("hello sarah")
+print("HELLO SARAH")
+print("hello sarah \nHello sarah")
+print("Hello" + " Sarah")
+print("Hello" + " " + "Sarah")
+print("Hello" + "" + "Sarah")
+print(f"{X} {Y}") #F-string allows you to format selected parts of a string.
