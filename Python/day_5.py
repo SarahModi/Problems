64231f4a7b47b0fa119b37dd342a85ab539c001e
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-10 23:37:09 +0530

    Update add user inputs

---
+++ Python/day_5.py
@@ -0,0 +1,4 @@
+print("Thanks for using our product")
+petname = input("what's the name of your pet? ")
+cityname = input("what city where you born in? ")
+print(f"Your new twitter handle and bio @cyber{petname} from {cityname}")
