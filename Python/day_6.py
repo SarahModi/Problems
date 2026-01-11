ac1a0804639877a7b34f2e9929eeb19bcdff013b
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-12 01:02:11 +0530

    Create day_6.py

---
+++ Python/day_6.py
@@ -0,0 +1,30 @@
+# Data types
+# Strings 
+#print(type("Hello World"))
+
+#Sub-scripting
+#print("Hello world"[0])
+
+#Integer
+#print(type(5))
+
+#Float
+#print(type(5.156))
+
+#Boolean
+#True or False
+#print()
+
+#Project: check which number is bigger from the given two numbers
+fnum = input("How old are you? " )
+lnum = input("How old is your mum? ")
+
+if fnum > lnum:
+ print("you are elder to your mum?:|")
+  
+elif fnum < lnum:
+  print("Phew...")
+
+else:
+ print("Wait what D:")
+     
