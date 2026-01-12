c1ce50f90d7f2d9768bc74be1142a87e02df4217
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-12 20:34:35 +0530

    Create day_9.c

---
+++ day_9.c
@@ -0,0 +1,13 @@
+// VALID STATEMENTS
+#include <stdio.h>
+
+int main(){
+  int a = 22
+  int b = a
+  int c = b * 10;
+  int d = 1, e;
+return 0;
+}
+
+// the compiler reads line by line, so never use a variable before declaring it
+// you cannot use and declare a variable at the same time
