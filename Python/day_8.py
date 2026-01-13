de5895fca5b0c3cda74e3284fa857d3567e063aa
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-14 00:35:57 +0530

    Create day_8.py

---
+++ Python/day_8.py
@@ -0,0 +1,20 @@
+#fruits = ["banana", "orange", "cherry"]
+
+#for fruit in fruits:
+#  print(fruit + " pie")
+
+#FOR LOOP
+for num in range (1,5):
+  print(num)
+
+for num in range (0,5):
+  print(num)
+
+#Find all numbers divisible by 3 from 1-100
+for num in range (1,100,3):
+  print(num)
+
+#FOR LOOP WITH IF STATEMENT
+for num in range (1,100):
+  if num % 3 == 0
+   print(num)
