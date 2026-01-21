69a3563fff6d83145e1a321463ac328940a8db85
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-21 12:22:03 +0530

    For and While Loops day_12.py

---
+++ Python/day_12.py
@@ -0,0 +1,26 @@
+# for loops
+for i in range (14):
+    print("This is Sarah.")
+
+
+
+# Creating a while loop
+max_devices = 10
+i = 1
+
+while i < max_devices:
+    print("user can still connect to additional devices")
+    i = i + 1
+print("user has reached maximum number of connected devices")
+
+# While loop output:
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user can still connect to additional devices
+user has reached maximum number of connected devices
