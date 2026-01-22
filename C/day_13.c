a41c355fcbd70d4e007253e846656d066f099e10
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-22 10:58:12 +0530

    Practice questions on operator precedence Create day_13.c

---
+++ C/day_13.c
@@ -0,0 +1,57 @@
+//Practice Q 
+// 1. 5*2 - 2*3
+
+#include <stdio.h>
+
+int main(){
+ int a = 5*2 - 2*3;
+  printf("result %d", a);
+
+  return 0;
+}
+
+
+
+// 2. 5*2/2*3
+
+#include <stdio.h>
+
+int main(){
+ int a = 5*2 / 2*3;
+  printf("result %d", a);
+
+  return 0;
+}
+
+
+
+// 3. 8*(2/2)*4
+
+#include <stdio.h>
+
+int main(){
+ int a = 8*(2/2)*4;
+  printf("result %d", a);
+
+  return 0;
+}
+
+
+//4. 3+3 / 7*7
+#include <stdio.h>
+
+int main(){
+ int a = 3+3 / 7*7;
+  printf("result %d", a);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+ 
