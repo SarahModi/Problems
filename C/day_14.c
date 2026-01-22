66f7db56ae0699b75713ed32c7ca8ae818439332
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-22 11:11:29 +0530

    Create day_14.c

---
+++ C/day_14.c
@@ -0,0 +1,25 @@
+//Modular Operator (returns remainder for int)
+
+#include <stdio.h>
+
+int main(){
+printf("%d", 16%10);
+
+  return 0;
+}
+
+
+
+//Modular Operator (returns remainder for int)
+//Output: 6
+
+#include <stdio.h>
+
+int main(){
+printf("%d", 1.6%1.0);
+
+  return 0;
+}
+
+// Moldulo operator doesn;t work on float datatypes
+ 
