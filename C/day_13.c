28b4f3adabd062961a50b4401103d73d1e67510c
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-22 11:16:26 +0530

    Update day_13.c

--- C/day_13.c
+++ C/day_13.c
@@ -12,40 +12,6 @@ int main(){
 
 
 
-// 2. 5*2/2*3
-
-#include <stdio.h>
-
-int main(){
- int a = 5*2 / 2*3;
-  printf("result %d", a);
-
-  return 0;
-}
-
-
-
-// 3. 8*(2/2)*4
-
-#include <stdio.h>
-
-int main(){
- int a = 8*(2/2)*4;
-  printf("result %d", a);
-
-  return 0;
-}
-
-
-//4. 3+3 / 7*7
-#include <stdio.h>
-
-int main(){
- int a = 3+3 / 7*7;
-  printf("result %d", a);
-
-  return 0;
-}
 
 
 
