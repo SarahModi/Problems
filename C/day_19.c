b3e1210ea72e7310bf22b4e85c24b857da9b02cb
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-28 19:33:31 +0530

    Create day_19.c Logical || OR

---
+++ C/day_19.c
@@ -0,0 +1,13 @@
+/*
+|| OR: Even if 1 statement if found to be true, the whole output is true
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  printf("%d \n", 4>5 || 5>2);
+
+  return 0;
+}
+
