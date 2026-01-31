a79dacc854184cc4e57bc4da683dbbe248d0270b
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-31 10:05:19 +0530

    Create day_15.py

---
+++ Python/day_15.py
@@ -0,0 +1,15 @@
+#greet by name
+
+def greet(name):
+  print("Welcome", name)
+
+greet ("Sarah")
+#Output
+# Welcome Sarah
+
+#-------------------------------------------------------------------
+
+def greet(fname, lname):
+  print("Welcome", fname, lname)
+
+greet ("Sarah", "Modi")
