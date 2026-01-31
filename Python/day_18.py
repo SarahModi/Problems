80ab7ed26c88825433691fd7f14bd28df4f36e5d
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-31 13:59:06 +0530

    Add day_18.py for exploring print and functions

---
+++ Python/day_18.py
@@ -0,0 +1,23 @@
+# Exploring input and ouput of print
+
+print("this is a string but", 27 , "is a number.")
+
+# Explore input and output of types
+print(type ("security"))
+print(type (75.2))
+
+# Exlpore max function
+a = 16
+b = 12
+c = 19
+print (max(a, b, c))
+# output
+# 19 
+# max outputs the highest value from the given
+
+# Exploring sorted function
+usernames = ["Sarah", "Rachael", "Anna", "Oliver", "Stephani", "Sophia", "Peppa"]
+print(sorted(usernames)) # pass the variable (usernames) through the sorted function
+
+#Output
+#['Anna', 'Oliver', 'Rachael', 'Sarah', 'Sophia', 'Stephani', 'Peppa']
