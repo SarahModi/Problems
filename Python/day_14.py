2ace2a9eb959793af8fafc9e388121e1b5f624e4
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-01-31 09:38:06 +0530

    Implement employee greeting function
    Added a function to greet the employee upon login.

---
+++ Python/day_14.py
@@ -0,0 +1,13 @@
+#FUNCTIONS
+#Define a function
+def greet_employee():
+  print("You've logged in successfully") #this line is indented because it is part of this function
+
+# the above code won't run so far cause you havnt CALLED your function YET
+
+#Call function
+greet_employee()
+
+#OUTPUT
+#You've logged in successfully
+  
