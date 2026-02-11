8db32014c35f441d3b91dba5149f460647455122
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-11 15:30:43 +0530

    Add for loop example in day_41.c

---
+++ C/day_41.c
@@ -0,0 +1,18 @@
+/*
+For Loops
+Syntax:
+
+for(initialisation; condition; updation){
+
+
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+  for(int i=10; i<=100; i=i+1){
+   printf("%d \n", i);
+  }
+  return 0;
+}
