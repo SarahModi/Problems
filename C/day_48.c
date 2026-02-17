462deba2da2c6f27c4b649b3d239edc59b019138
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-17 14:45:13 +0530

    Implement loop to print lowercase letters
    Added a loop to print all lowercase letters from 'a' to 'z'.

---
+++ C/day_48.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    for (char ch='a'; ch<='z'; ch++){
+      printf("%c\n", ch);
+    }
+    return 0;
+}
+
+//OUTPUT
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
