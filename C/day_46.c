fa4c997c162a89c4d8ada35836f4f3ba69dafdc7
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-17 14:27:35 +0530

    Fix decrement operator comments and printf usage
    Corrected comments and updated printf statements to reflect post-decrement usage.

---
+++ C/day_46.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //decrement operators
+    //--i(pre decrement) 
+    //i--(post decrement)
+    int i = 0;
+    printf("%d\n", i--); //decrease i, then use
+    printf("%d\n", i);
+
+    return 0;
+}
+
+//OUTPUT
+-1
+-1
