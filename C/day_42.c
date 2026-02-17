edec3e8c5295b39f88c36db12a7db2dbd11a8349
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-17 14:06:05 +0530

    Add for loop to print numbers from 0 to 10
    Implement a for loop to print numbers from 0 to 10.

---
+++ C/day_42.c
@@ -0,0 +1,23 @@
+//Print numbers from 0-10 using for loop
+#include <stdio.h>
+
+int main() {
+    for (int i=0; i<=10; i=i+1){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+//OUTPUT
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
