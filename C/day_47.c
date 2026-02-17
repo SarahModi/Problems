d32bfca75638ff278833cba367c0c0e6bd1f032f
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-17 14:41:56 +0530

    Add for loop to print float numbers
    Implement a for loop to print float values from 1.0 to 5.0.

---
+++ C/day_47.c
@@ -0,0 +1,15 @@
+//FOR LOOP on float
+#include <stdio.h>
+
+int main() {
+    for (float i=1.0; i<=5.0; i++){
+      printf("%f\n", i);
+    }
+    return 0;
+}
+//OUTPUT
+1.000000
+2.000000
+3.000000
+4.000000
+5.000000
