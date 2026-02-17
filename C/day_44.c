a9f3e7e7c6a4a28455f1a4402b859ef99a4c9536
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-02-17 14:20:55 +0530

    Add example of increment operators in C
    Demonstrate increment operators in C with example output.

---
+++ C/day_44.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //increment operators
+    //i++ (POST INCREMENT) 
+    //++i (PRE INCREMENT)
+    int i = 1;
+    printf("%d\n", ++i); //increase i, then use
+    printf("%d\n", i);
+
+    return 0;
+}
+
+//OUTPUT
+2
+2
