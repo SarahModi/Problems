187ad8a77340cd1cbe06fbe92677bb2d89c4dfc9
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-04 15:51:15 +0530

    Create day_27.py

---
+++ Python/day_27.py
@@ -0,0 +1,11 @@
+# Extract a slice from a string
+print("SARAH"[1:4])
+
+#Output:
+ARA
+
+
+print("SARAH".index("R")) #.index finds the first occurrence of the input in a string and returns it's location
+
+#Output
+2
