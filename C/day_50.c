1ff4bcdd20e6bf301d291f8545ca8d64ced736e9
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-04 18:17:33 +0530

    Create day_50.c

---
+++ C/day_50.c
@@ -0,0 +1,18 @@
+//While loop
+#include <stdio.h>
+int main() {
+  int i=1;
+  while(i<=5) {
+    printf("You are Hacked \n");
+    i++;
+  }
+  return 0;
+}
+
+/* Output 
+You are Hacked 
+You are Hacked 
+You are Hacked 
+You are Hacked 
+You are Hacked 
+*/
