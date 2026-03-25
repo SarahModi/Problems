94a9094444701de1c3accb18ed9dffa78c77a739
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-25 19:35:39 +0530

    Create day_66.c

---
+++ C/day_66.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+#define A 18
+int sarah = 19;
+void coffee();
+
+int main(){
+printf("%d\n",A);
+printf("%d\n",sarah);
+coffee();
+    return 0;
+}
+void coffee()
+{
+    int sarah = 14;
+    printf("%d",sarah);
+}
+//you can decalre one more int named the same "sarah" but inside our coffee block only
