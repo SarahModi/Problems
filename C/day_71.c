9b4b345c8f3f9b6b0265d87f00d5946359a57c57
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-25 19:38:54 +0530

    Create day_71.c

---
+++ C/day_71.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int a = 10, b = 20, c= 30;
+
+if (a > b && a > c)
+ printf("a is greater than b and c");
+else
+ printf("a is not greater than b and c");
+ 
+return 0;
+}
