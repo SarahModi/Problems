b1f9c0832c4c27d1779fda913a01ff453848c550
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-25 19:37:47 +0530

    Create day_69.c

---
+++ C/day_69.c
@@ -0,0 +1,27 @@
+// leap year
+#include <stdio.h>
+
+int main(){
+int year;
+
+printf("Enter a year: ");
+scanf("%d", &year);
+
+if (year % 4 == 0)
+ {
+     if (year % 100 == 0)
+     {
+         if (year % 400 == 0)
+          printf("Leap year");
+        else
+          printf("Not a leap year");
+     }
+     else
+      printf("Leap year");
+      
+    }
+    else
+     printf("Not a leap year");
+    
+return 0;
+}
