4cb840b6be65d27d74abdbcd946825533f6bc500
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-03-30 13:48:57 +0530

    day_80.c
    pointers (%p)

--- C/Foundation/day_80.c
+++ C/Foundation/day_80.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-  
+    int var = 450;
+    int *ptr = &var; 
+
+    printf("%p", *ptr);
+    return 0;
 }
+/*
+Change the format specifier to p from d and it will give you the memory address of a variable in hexadecimal format.
+*/
