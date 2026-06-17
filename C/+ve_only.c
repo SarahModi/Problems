dcdfdc75aa02f3321843d514a2d3b331f59ed2d4
Sarah Modi <162476528+SarahModi@users.noreply.github.com>
2026-06-17 07:46:51 -0400

    Add files via upload

---
+++ C/+ve_only.c
@@ -0,0 +1,31 @@
+/*
+Count Positive Numbers.
+Ask the user to enter 5 numbers.
+and the prgoram tells the number of positive numbers in the users input
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int num;
+int positive_count = 0;
+
+printf("Enter 5 numbers:");
+
+
+for (int i = 0; i < 5; i++)
+{
+
+  scanf("%d", &num);
+
+
+  if (num>0)
+  {
+  positive_count++;
+  printf("Positive count = %d\n", positive_count);
+  }
+}
+
+return 0;
+}
---
+++ C/+veor-ve.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a;
+
+printf("enter a number: ");
+scanf("%d", &a);
+
+if (a>0){
+    printf("positive");
+
+}
+else if(a<0){
+    printf("negative");
+}
+
+else{
+    printf("zero");
+}
+
+return 0;
+
+}
